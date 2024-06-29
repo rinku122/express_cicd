79cf18c44e734d0d08bac94cab2ed6242f5a1bdd
Rajesh <rajesh.kumar@antiersolutions.com>
2024-06-29 22:50:51 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req: any, res: any) => {
+  res.send("Hello World!");
+});
+
+app.get("/bye", (req: any, res: any) => {
+  res.send("Bye World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
